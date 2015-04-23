e7d7801e87ef0d16ec7dd68a03a074f641adb0b9
Dirk-Jan van de Sanden <d.vdsanden@audiomodules.net>
2015-04-23 10:26:45 +0200

    Adds reveal on scroll functionality to the theme

--- js/loadicons.js
+++ js/loadicons.js
@@ -1,7 +1,7 @@
 $(window).load(function(){
 	if (document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1")) {
 		$( ".icon-names" ).each(function() {
-			var html='<div class="product-icons">';
+			var html='<div data-timeout="500" class="product-icons revealOnScroll">';
 			var classname = $(this).text().split(' ');
 
 			for (var j in classname){
--- js/scroll.js
+++ js/scroll.js
@@ -1,3 +1,8 @@
+var $window = $(window),
+    win_height_padded = $window.height() * 1.1;
+//       isTouch           = Modernizr.touch;
+
+
 $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -17,12 +22,33 @@ $(function() {
 
 var num = 200; //number of pixels before modifying styles
 
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() > num) {
+$window.bind('scroll', function() {
+    var scrolled = $window.scrollTop();
+
+    if (scrolled > num) {
         $('#access').addClass('fixed');
         $('#access').prev().addClass('menucompensation');
     } else {
         $('#access').removeClass('fixed');
         $('#access').prev().removeClass('menucompensation');
     }
+
+    $(".revealOnScroll:not(.animated)").each(function() {
+        var $this = $(this),
+            offsetTop = $this.offset().top;
+
+        if (scrolled + win_height_padded > offsetTop) {
+            if ($this.data('timeout')) {
+                setTimeout(function() {
+                    $this.addClass('animated');
+                }, parseInt($this.data('timeout'), 10));
+            } else {
+                setTimeout(function() {
+                    $this.addClass('animated');
+                }, 500);
+            }
+        }
+    });
 });
+
+
